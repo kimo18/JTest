a0f07b7e167ad269f620e2a23bbde7d53acf5ac7
Karim Badreldin <kimoalaa18@yahoo.com>
2023-02-17 16:09:11 +0100

    new stage

--- untitled/src/HWorld.groovy
+++ untitled/src/HWorld.groovy
@@ -7,5 +7,18 @@ pipeline{
                     echo 'Hello World'
                 }
             }
+            stage('Hello') {
+                steps {
+                    int var = 20
+                     if(var<0){
+
+                     echo 'number is less than Zero'
+                     }
+                    else{
+                         echo ' number is greater than Zero'
+                     }
+                }
+
+            }
         }
     }
